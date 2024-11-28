e53980719546baa4558ef200e28b1b700c76a0b9
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-11-29 00:35:10 +0600

    solve 5 problems

---
+++ index.js
@@ -0,0 +1,54 @@
+//1. Print "Hello, World!" to the console.
+
+
+//console.log("Hello, World!");
+
+//2. Declare a variable and assign it a value.
+
+
+
+//let myVariable = 10;
+
+
+//3. Write a function to return the sum of two numbers.
+
+
+
+function sumOfTwo() {
+    let a = 10,
+        b = 30;
+
+    return a + b;
+}
+// let output = sumOfTwo();
+// console.log(output);
+
+
+//4. Create a constant and try reassigning it (observe the error).
+
+
+const number = 23;
+
+// number = 31;
+// console.log(number);
+
+
+//5. Write a function to check if a number is even or odd.
+
+
+
+function findEvenOrOdd(n) {
+    if (n % 2 == 0) {
+        return "Even";
+
+    } else {
+        return "Odd";
+    }
+}
+
+// let result = findEvenOrOdd(11);
+// console.log(result);
+
+
+
+//6. Create an array and access its first and last elements.
